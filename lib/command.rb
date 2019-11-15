d2e5745e9aef2fc5694d73e608b7a47670e2652e
Eric Garside <garside@gmail.com>
2019-11-15 00:25:52 -0500

    Initial Commit

---
+++ lib/command.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require "command/version"
+
+module Command; end
---
+++ spec/command_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Command do
+  it "has a version number" do
+    expect(Command::VERSION).not_to be nil
+  end
+end
