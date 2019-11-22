ba2795a24c3d15bbcb395005812dbc5b3fac9eba
Eric Garside <garside@gmail.com>
2019-11-22 13:30:23 -0500

    Command::Malfunction
    - Implement malfunction

--- spec/command/field_error_spec.rb
+++ spec/command/field_error_spec.rb
@@ -1,47 +1,65 @@
 # frozen_string_literal: true
 
 RSpec.describe Command::FieldError, type: :field_error do
-  subject { described_class }
+  subject(:field_error) { described_class.new(field_name: field_name, error_code: error_code) }
 
-  let(:instance) { described_class.new(**input_hash) }
-  let(:input_hash) do
-    { field_name: field_name, error_code: error_code, message: message }
-  end
-  let(:field_name) { Faker::Lorem.word }
-  let(:error_code) { Faker::Lorem.word }
-  let(:message) { Faker::Lorem.sentence }
+  let(:field_name) { Faker::Internet.domain_word }
+  let(:error_code) { Faker::Internet.domain_word }
 
   it { is_expected.to inherit_from Spicerack::InputObject }
 
   it { is_expected.to define_argument :field_name }
   it { is_expected.to define_argument :error_code }
-  it { is_expected.to define_argument :message }
 
-  describe ".collection_from_json" do
-    subject { described_class.collection_from_json field_error_hashes }
-
-    let(:field_error_hashes) { [ input_hash ] }
-
-    before { allow(described_class).to receive(:new).with(input_hash).and_return(instance) }
-
-    it { is_expected.to match_array [ instance ] }
-  end
+  it { is_expected.to alias_method(:eql?, :==) }
 
   describe "#field_name" do
-    subject { instance.field_name }
+    subject { field_error.field_name }
 
     it { is_expected.to eq field_name }
   end
 
   describe "#error_code" do
-    subject { instance.error_code }
+    subject { field_error.error_code }
 
     it { is_expected.to eq error_code }
   end
 
-  describe "#message" do
-    subject { instance.message }
+  describe "#==" do
+    context "when other is nil" do
+      let(:other) { nil }
 
-    it { is_expected.to eq message }
+      it { is_expected.not_to eq other }
+    end
+
+    context "when other is nonsense" do
+      let(:other) { SecureRandom.hex }
+
+      it { is_expected.not_to eq other }
+    end
+
+    context "when blank other" do
+      let(:other) { described_class.new(field_name: nil, error_code: nil) }
+
+      it { is_expected.not_to eq other }
+    end
+
+    context "when other with different data" do
+      let(:other) { described_class.new(field_name: error_code, error_code: field_name) }
+
+      it { is_expected.not_to eq other }
+    end
+
+    context "when other with same data" do
+      let(:other) { described_class.new(field_name: field_name, error_code: error_code) }
+
+      it { is_expected.to eq other }
+    end
+
+    context "when other is exactly the same" do
+      let(:other) { field_error }
+
+      it { is_expected.to eq other }
+    end
   end
 end
--- spec/command/malfunction_spec.rb
+++ spec/command/malfunction_spec.rb
@@ -8,9 +8,131 @@ RSpec.describe Command::Malfunction, type: :malfunction do
     example_command.malfunction
   end
 
+  it { is_expected.to inherit_from Spicerack::RootObject }
+
+  it { is_expected.to delegate_method(:flow_state).to(:command) }
+
   describe "#command" do
     subject { example_malfunction.command }
 
     it { is_expected.to eq example_command }
+  end
+
+  describe "#problem" do
+    subject { example_malfunction.problem }
+
+    context "with state errors" do
+      let(:example_state_class) do
+        Class.new(Flow::StateBase) do
+          option :username
+          validates :username, presence: true
+        end
+      end
+
+      before { example_command.execute }
+
+      it { is_expected.to eq :invalid_state }
+    end
+
+    context "with operation failure" do
+      let(:problem) { Faker::Internet.domain_word.to_sym }
+      let(:operation_failure) { instance_double(Flow::Operation::Failures::OperationFailure, problem: problem) }
+
+      before { allow(example_command).to receive(:operation_failure).and_return(operation_failure) }
+
+      it { is_expected.to eq problem }
+    end
+
+    context "without either" do
+      it { is_expected.to eq :unknown_error }
+    end
+  end
+
+  describe "#details" do
+    subject { example_malfunction.details }
+
+    context "with state errors" do
+      let(:example_state_class) do
+        Class.new(Flow::StateBase) do
+          option :username
+          validates :username, presence: true
+        end
+      end
+
+      before { example_command.execute }
+
+      it { is_expected.to eq(field_errors: [ Command::FieldError.new(field_name: :username, error_code: :blank) ]) }
+    end
+
+    context "with operation failure" do
+      let(:details) { Hash[*Faker::Lorem.words(2 * rand(1..2))] }
+      let(:operation_failure) { instance_double(Flow::Operation::Failures::OperationFailure, details: details) }
+
+      before { allow(example_command).to receive(:operation_failure).and_return(operation_failure) }
+
+      it { is_expected.to eq details }
+
+      context "with extra field errors" do
+        let(:field_errors) { [ Command::FieldError.new(field_name: :other_field_name, error_code: :other_error_code) ] }
+
+        before { example_malfunction.add_field_error :other_field_name, :other_error_code }
+
+        it { is_expected.to eq details.merge(field_errors: field_errors) }
+      end
+    end
+
+    context "without either" do
+      it { is_expected.to be_empty }
+    end
+  end
+
+  describe "#field_errors?" do
+    before { allow(example_malfunction).to receive(:field_errors).and_return(field_errors) }
+
+    context "with field errors" do
+      let(:field_errors) { [ double ] }
+
+      it { is_expected.to be_field_errors }
+    end
+
+    context "without field errors" do
+      let(:field_errors) { [] }
+
+      it { is_expected.not_to be_field_errors }
+    end
+  end
+
+  describe "#field_errors" do
+    subject { example_malfunction.field_errors }
+
+    context "without state errors" do
+      it { is_expected.to be_blank }
+    end
+
+    context "with state errors" do
+      let(:example_state_class) do
+        Class.new(Flow::StateBase) do
+          option :username
+          option :password
+          validates :username, presence: true
+          validates :password, presence: true
+        end
+      end
+
+      let(:no_username_error) { Command::FieldError.new(field_name: :username, error_code: :blank) }
+      let(:no_password_error) { Command::FieldError.new(field_name: :password, error_code: :blank) }
+
+      before { example_command.execute }
+
+      context "with multiple errors" do
+        it { is_expected.to eq [ no_username_error, no_password_error ] }
+      end
+
+      context "with one error" do
+        let(:input) { Hash[:username, SecureRandom.hex] }
+
+        it { is_expected.to eq [ no_password_error ] }
+      end
+    end
   end
 end
